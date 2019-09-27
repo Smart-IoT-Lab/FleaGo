0df4f6ecf514e82edc7d17fe37ad07f10c96001a
YoungAndRich <47108798+YoungAndRich@users.noreply.github.com>
2019-09-27 15:41:12 +0900

    거리 순 정렬, 운영시간 출력

--- app/src/main/java/com/example/fleago/Market.java
+++ app/src/main/java/com/example/fleago/Market.java
@@ -9,15 +9,32 @@ public class Market {
     private String name;
     private String page_url;
     private String location;
-    ArrayList<String> event_type ;
+    ArrayList<String> event_type =  new ArrayList<>();
+    ArrayList<String> gps = new ArrayList<>();  // 0:경도, 1:위도
+    private String start_time;
+    private String end_time;
 
+    private int distance;
+
+    public int getDistance() {
+        return distance;
+    }
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
 
     public String getLocation() {
         return location;
     }
-
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public ArrayList<String> getGps() {
+        return gps;
+    }
+    public void setGps(ArrayList<String> gps) {
+        this.gps = gps;
     }
 
     public void setDistrict(String district) {
@@ -32,10 +49,10 @@ public class Market {
     public void setName(String name) {
         this.name = name;
     }
-
     public void setPage_url(String page_url) {
         this.page_url = page_url;
     }
+
     public String getDistrict() {
         return district;
     }
@@ -51,7 +68,12 @@ public class Market {
     public String getPage_url() {
         return page_url;
     }
-
+    public String getEnd_time() {
+        return end_time;
+    }
+    public String getStart_time() {
+        return start_time;
+    }
 
     public Market(){ }
 
@@ -61,10 +83,35 @@ public class Market {
         this.introduction=introduction;
         this.name=name;
         this.page_url=page_url;
-
     }
 
     public String toString() {
         return name+ event_type;
+    }
+
+    public boolean hasGps() {
+        // gps 정보가 db에 있는가
+        if (this.gps == null) {
+            return false;
+        } else if (this.gps.size() == 0) {
+            return false;
+        }else if (this.gps.get(0).equals("N") || this.gps.get(1).equals("N")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean hasTime() {
+        // 운영시간에 대한 정보가 db에 있는가
+        if (this.start_time == null || this.end_time == null) {
+            return false;
+        } else if (this.start_time.length() == 0 || this.end_time.length() == 0) {
+            return false;
+        } else if (this.start_time.equals("None") || this.end_time.equals("None")) {
+            return false;
+        }
+
+        return true;
     }
 }
