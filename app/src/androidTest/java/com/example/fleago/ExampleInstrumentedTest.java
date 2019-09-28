46cdaf5eed543f45854a6ca501f510e794c4d75a
a0107476 <a01074761054@gmail.com>
2019-09-28 15:53:20 +0900

    프론트

--- app/src/androidTest/java/com/example/fleago/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/fleago/ExampleInstrumentedTest.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
@@ -17,7 +17,6 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
