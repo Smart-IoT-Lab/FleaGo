63226247133d6885ea19875362749a57b2f3543e
cokemania <coke_mania@naver.com>
2019-09-28 16:32:18 +0900

    시작화면 추가

---
+++ app/src/main/java/com/example/fleago/splash_screen.java
@@ -0,0 +1,52 @@
+package com.example.fleago;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        ActionBar ab = getSupportActionBar() ;
+        ab.hide();
+
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(2*1000);
+
+                    Intent intent = new Intent(splash_screen.this,MainActivity.class);
+                    startActivity(intent);
+
+                    // After 5 seconds redirect to another intent
+
+
+                    //Remove activity
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        // start thread
+        background.start();
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+    }
+
+}
